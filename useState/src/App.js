1fdbe2881b985ae4e6e23d9e63837ac9db29f271
= <=>
2021-03-21 19:58:23 +0530

    React useState example with array

--- useState/src/App.js
+++ useState/src/App.js
@@ -3,34 +3,31 @@ import {TextField , Icon, Button} from '@material-ui/core';
 
 
 const App = () => {
-   const [user, setUser] = useState({ name: '', email: ''});
-   const [submitted, setSubmitted] = useState(false);
-   const [message, setMessage] = useState('');
-   const handleSubmit = (e) => {
-       if(user.name && user.mail){
-          setSubmitted(true);
-       }
-       setMessage('please enter values');
-       return;
-
+   const [data, setData] = useState([]);
+   const [value, setValue] = useState('');
+   
+   const updateData = () => {
+     setData(data => [...data, {
+        id: '1',
+        value
+     }]);
+       
    }
 
    return(
       <form  noValidate autoComplete="off">
-  <TextField id="standard-basic" label="Name" value={user.name} onChange={(e) => setUser({ ...user,name:e.target.value})} /><br></br>
-  <TextField id="standard-basic" label="Email" value={user.email} onChange={(e) => setUser({...user, email:e.target.value})} /><br></br><br></br>
-   <Button
+  <TextField id="standard-basic" label="Name" value={value} onChange={(e) => setValue( e.target.value)} /><br></br> <br></br> <br></br>
+  <Button
         variant="contained"
         color="primary"
         endIcon={<Icon>send</Icon>}
-        onClick={handleSubmit}
+        onClick={updateData}
       >
-        Submit
+        Update
       </Button><br></br><br></br>
-      {user.name}<br></br>
-   
-      {user.email}<br></br>
-      {message}
+      {data.map((i,index) => {
+         return <li key={index}>{i.id}</li>
+      })}
   
 </form>
      
